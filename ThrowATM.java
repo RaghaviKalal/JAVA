48e82fe298bbb7f8a7d0eabdcb1f8f24c684be80
RaghaviKalal <raghavirrk2007@gmail.com>
2026-09-24 22:14:30 +0530

    Add files via upload

---
+++ ThrowATM.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ThrowATM {
+
+    static void verifyPIN(int pin) throws Exception {
+        if (pin != 1234) {
+            throw new Exception("Invalid PIN");
+        }
+        System.out.println("PIN Verified Successfully");
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter ATM PIN: ");
+        int pin = sc.nextInt();
+
+        try {
+            verifyPIN(pin);
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            System.out.println("PIN verification process completed.");
+        }
+
+        sc.close();
+    }
+}
